f191a460e15a0b66c8c65befa48a728e6363f32d
yihengsir <32477451+yihengsir@users.noreply.github.com>
2025-06-14 00:54:20 +0000

    Fix vite config and rebuild for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/duolingo-prep-platform/', // 👈 注意：这是你的 GitHub 仓库名，必须加上！
+  base: '/duolingo-prep-platform/', // 注意：仓库名，必须加上！
   plugins: [react()],
 });
