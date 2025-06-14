f090f2110e48b84118e04884284b1c4047217f2c
yihengsir <32477451+yihengsir@users.noreply.github.com>
2025-06-14 00:26:15 +0000

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 // vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/duolingo-prep-platform/', // 👈 这是关键
+  base: '/duolingo-prep-platform/', // ✅ 注意这里要加上你的 repo 名
   plugins: [react()],
 })
