12a2a187cceda7a809973790626611d97a920fd9
yihengsir <32477451+yihengsir@users.noreply.github.com>
2025-06-14 08:27:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/duolingo-prep-platform/', // ✅ 注意这里要加上你的 repo 名
   plugins: [react()],
-  // 移除 css.postcss 配置，让 Vite 自动查找项目根目录的 postcss.config.cjs
-  // 这样可以避免在 ESM 环境的 vite.config.js 中使用 CommonJS 的 require()
-});
+})
